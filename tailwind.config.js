58b574059f442da7b5161f56d4a2c55ed1ac61c0
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2022-01-14 14:17:51 +0600

    basic stracture design Done

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: "jit",
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "body-text": "rgb(33,37,41)",
+        "body-100": "rgb(229,241,251)",
+        "body-200": "#fff",
+        "body-300": "rgb(255,247,228)",
+        "blue-1100": "rgb(86,165,247)",
+        "blue-1200": "rgb(162,186,245)",
+        "blue-light": "rgb(78,226,198)",
+        "blue-dark": "rgb(70,166,224)",
+        "pink-1100": "rgb(255,179,213)",
+        "yello-1100": "rgb(252,225,208)",
+        "gray-1100": "rgb(69,103,131)",
+        "gray-1200": "rgb(69,103,131)",
+      },
+    },
+  },
+  plugins: [],
+};
